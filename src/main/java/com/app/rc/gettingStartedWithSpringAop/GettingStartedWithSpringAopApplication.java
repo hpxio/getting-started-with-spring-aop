6b1a285b3dd1493fbfe780db023e6fe1b4b905de
iHSPA <harrsh.hp@live.in>
2019-08-17 00:46:12 +0530

    App-Log::Init-Project

---
+++ src/main/java/com/app/rc/gettingStartedWithSpringAop/GettingStartedWithSpringAopApplication.java
@@ -0,0 +1,13 @@
+package com.app.rc.gettingStartedWithSpringAop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithSpringAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/rc/gettingStartedWithSpringAop/GettingStartedWithSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.rc.gettingStartedWithSpringAop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
